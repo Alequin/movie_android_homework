1eb9b29e30be76b692648dc3f2f2ef495cdb9c68
James Cox <j-cox03@hotmail.com>
2017-07-03 18:09:27 +0100

    RankingsTest: made before method

--- test/java/com/example/james/movieandroidhomework/RankingsTest.java
+++ test/java/com/example/james/movieandroidhomework/RankingsTest.java
@@ -1,8 +1,36 @@
 package com.example.james.movieandroidhomework;
 
+import org.junit.Test;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
 /**
  * Created by james on 03/07/2017.
  */
 
 public class RankingsTest {
+
+    private Rankings rankings;
+
+    @Before
+    public void setup(){
+        Movie[] movies = {
+            new Movie("The Wizard Of Oz", Genre.FANTASY),
+            new Movie("Death Note", Genre.MYSTERY),
+            new Movie("Your Name", Genre.ROMANCE),
+            new Movie("The Boy and the Beast", Genre.ACTION),
+            new Movie("Lord of the Rings", Genre.FANTASY),
+            new Movie("Forest Gump", Genre.COMEDY),
+            new Movie("Star Wars", Genre.SCIENCE_FICTION),
+            new Movie("Back to The Future", Genre.SCIENCE_FICTION),
+            new Movie("Inception", Genre.ACTION),
+            new Movie("Goodfellas", Genre.DRAMA),
+            new Movie("The Lion King", Genre.ADVENTURE)
+        };
+        rankings = new Rankings(movies);
+    }
+
+
+
 }
