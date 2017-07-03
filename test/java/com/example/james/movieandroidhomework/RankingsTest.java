5aa50c0ccfe5568b408bcc022d915bccbaa92c4d
James Cox <j-cox03@hotmail.com>
2017-07-03 20:55:12 +0100

    MovieTest: made test to get movie by ranking

--- test/java/com/example/james/movieandroidhomework/RankingsTest.java
+++ test/java/com/example/james/movieandroidhomework/RankingsTest.java
@@ -12,10 +12,12 @@ import static org.junit.Assert.*;
 public class RankingsTest {
 
     private Rankings rankings;
+    private Movie[] movies;
+    private Movie extraMovie;
 
     @Before
     public void setup(){
-        Movie[] movies = {
+        Movie[] temp = {
             new Movie("The Wizard Of Oz", Genre.FANTASY),
             new Movie("Death Note", Genre.MYSTERY),
             new Movie("Your Name", Genre.ROMANCE),
@@ -25,12 +27,22 @@ public class RankingsTest {
             new Movie("Star Wars", Genre.SCIENCE_FICTION),
             new Movie("Back to The Future", Genre.SCIENCE_FICTION),
             new Movie("Inception", Genre.ACTION),
-            new Movie("Goodfellas", Genre.DRAMA),
-            new Movie("The Lion King", Genre.ADVENTURE)
+            new Movie("Goodfellas", Genre.DRAMA)
         };
+        movies = temp;
+        extraMovie = new Movie("Goodfellas", Genre.DRAMA);
         rankings = new Rankings(movies);
     }
 
-
+    @Test
+    public void getMovieByRanking(){
+        Movie result = null;
+        result = rankings.getMovieAtRank(1);
+        assert(movies[0].equals(result));
+        result = rankings.getMovieAtRank(2);
+        assert(movies[1].equals(result));
+        result = rankings.getMovieAtRank(3);
+        assert(movies[2].equals(result));
+    }
 
 }
