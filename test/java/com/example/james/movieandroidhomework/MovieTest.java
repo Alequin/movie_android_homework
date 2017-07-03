d4332aeb76935c2e55d5977dbff2bef15db556a0
James Cox <j-cox03@hotmail.com>
2017-07-03 17:24:04 +0100

    MovieTest: made tests

---
+++ test/java/com/example/james/movieandroidhomework/MovieTest.java
@@ -0,0 +1,76 @@
+package com.example.james.movieandroidhomework;
+
+import org.junit.Test;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by james on 03/07/2017.
+ */
+
+public class MovieTest {
+
+    private Movie movie;
+
+    @Before
+    public void setup(){
+        movie = new Movie("The Movie", Genre.ACTION);
+    }
+
+    @Test
+    public void canGetDetailsFromToStringWhenRankingZero(){
+        String expected = "Title - The Movie, Genre - action, Ranking - no ranking";
+        String result = movie.toString();
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testRankingStartsAtZero(){
+        assertEquals(0, movie.getRanking());
+    }
+
+    @Test
+    public void resetRanking(){
+        movie.resetRanking();
+        assertEquals(0, movie.getRanking());
+    }
+
+    @Test
+    public void canSetRankingBetweenOneAndTen(){
+        for(int rank=1; rank <=10; rank++){
+            movie.setRanking(rank);
+            assertEquals(rank, movie.getRanking());
+        }
+    }
+
+    @Test
+    public void cannotSetRankingLessThanOne(){
+        boolean pass = false;
+        try {
+            movie.setRanking(0);
+        }catch(IllegalArgumentException ex){
+            pass = true;
+        }
+        assert(pass);
+    }
+
+    @Test
+    public void cannotSetRankingMoreThan10(){
+        boolean pass = false;
+        try {
+            movie.setRanking(11);
+        }catch(IllegalArgumentException ex){
+            pass = true;
+        }
+        assert(pass);
+    }
+
+    @Test
+    public void canGetDetailsFromToStringWhenRankingfive(){
+        movie.setRanking(5);
+        String expected = "Title - The Movie, Genre - action, Ranking - 5";
+        String result = movie.toString();
+        assertEquals(expected, result);
+    }
+}
