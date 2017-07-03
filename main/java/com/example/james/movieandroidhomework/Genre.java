1eb66cc0b110d894636b1060f8a8f8aa26f77a5f
James Cox <j-cox03@hotmail.com>
2017-07-03 17:36:34 +0100

    Made Movie class and Genre enum. All current tests pass

---
+++ main/java/com/example/james/movieandroidhomework/Genre.java
@@ -0,0 +1,21 @@
+package com.example.james.movieandroidhomework;
+
+/**
+ * Created by james on 03/07/2017.
+ */
+
+enum Genre {
+
+    DRAMA,
+    HORROR,
+    THRILLER,
+    ACTION,
+    WESTERN,
+    ANIMATION,
+    ROMANCE,
+    WAR,
+    SCIENCE_FICTION,
+    FANTASY,
+    COMEDY
+
+}
--- main/java/com/example/james/movieandroidhomework/Movie.java
+++ main/java/com/example/james/movieandroidhomework/Movie.java
@@ -10,4 +10,51 @@ public class Movie {
     private Genre genre;
     private int ranking;
 
+    public Movie(String title, Genre genre){
+        this.title = title;
+        this.genre = genre;
+        this.setRanking(0);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public int getRanking() {
+        return ranking;
+    }
+
+    public void setRanking(int ranking) {
+        if(ranking < 0 || ranking > 10){
+            String message = "Ranking of " + ranking + " not valid";
+            throw new IllegalArgumentException(message);
+        }
+        this.ranking = ranking;
+    }
+
+    public void resetRanking() {
+        this.setRanking(0);
+    }
+
+    @Override
+    public String toString() {
+        String rankingText = "no ranking";
+        if(this.ranking != 0){
+            rankingText = Integer.toString(this.ranking);
+        }
+        String genreText = this.genre.toString().toLowerCase();
+        return "Title - " + title + ", Genre - " + genreText + ", Ranking - " + rankingText;
+    }
 }
