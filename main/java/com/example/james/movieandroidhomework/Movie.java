167870227d0cc269df022f4f1b5f7c2d6b43d115
James Cox <j-cox03@hotmail.com>
2017-07-03 16:57:47 +0100

    Movie: made class outline

---
+++ main/java/com/example/james/movieandroidhomework/Movie.java
@@ -0,0 +1,13 @@
+package com.example.james.movieandroidhomework;
+
+/**
+ * Created by james on 03/07/2017.
+ */
+
+public class Movie {
+
+    private String title;
+    private Genre genre;
+    private int ranking;
+
+}
